15b9e1728aa141eb77552c3e3798999612820811
Greg Mercer (GBear) <gmercer@stanford.edu>
2017-12-14 19:23:17 -0800

    Overriding the UpdateBackgroundColor method in  FixFrameRenderer. Oddly this seems to work. Not sure I understand why.

--- DataTemplates/Droid/Renderers/FixFrameRenderer.cs
+++ DataTemplates/Droid/Renderers/FixFrameRenderer.cs
@@ -1,89 +1,43 @@
 ﻿using System;
+using System.ComponentModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
 using DataTemplates.Droid.Renderers;
 using DataTemplates.Views;
-using System.ComponentModel;
+
 using Android.Graphics.Drawables;
+using Android.Content;
 
 [assembly: ExportRenderer(typeof(TimeSlotFrame), typeof(FixFrameRenderer))]
 namespace DataTemplates.Droid.Renderers
 {
-    public class FixFrameRenderer : VisualElementRenderer<Frame>
+    public class FixFrameRenderer : FrameRenderer
     {
+        public FixFrameRenderer(Context context) : base(context)
+        {
+        }
+
+        [Obsolete("This constructor is obsolete as of version 2.5. Please use FrameRenderer(Context) instead.")]
+        public FixFrameRenderer()
+        {
+            AutoPackage = false;
+        }
+
         protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
         {
             base.OnElementChanged(e);
-
-            if (e.NewElement != null)
-            {
-                var drawable = new GradientDrawable();
-                ViewGroup.SetBackground(drawable);
-                UpdateBackgroundColor(drawable);
-                UpdateCornerRadius(drawable);
-                UpdateOutlineColor(drawable);
-                UpdateShadow();
-            }
         }
 
         protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             base.OnElementPropertyChanged(sender, e);
-
-            var drawable = ViewGroup?.Background as GradientDrawable;
-            if (drawable != null)
-            {
-                if (e.PropertyName == VisualElement.BackgroundColorProperty.PropertyName)
-                {
-                    UpdateBackgroundColor(drawable);
-                } 
-                else if (e.PropertyName == Frame.CornerRadiusProperty.PropertyName)
-                {
-                    UpdateCornerRadius(drawable);
-                }
-                else if (e.PropertyName == Frame.OutlineColorProperty.PropertyName)
-                {
-                    UpdateOutlineColor(drawable);
-                }
-                else if (e.PropertyName == Frame.HasShadowProperty.PropertyName)
-                {
-                    UpdateShadow();
-                }
-            }
         }
 
         protected override void UpdateBackgroundColor()
         {
             // This method doesn't work well in Xamarin.Forms -Version 2.3.4.247
-        }
-
-        private void UpdateCornerRadius(GradientDrawable drawable)
-        {
-            drawable.SetCornerRadius(Element.CornerRadius);
-        }
-
-        private void UpdateOutlineColor(GradientDrawable drawable)
-        {
-            drawable.SetStroke(1, Element.OutlineColor.ToAndroid());
-        }
-
-        private void UpdateBackgroundColor(GradientDrawable drawable)
-        {
-            drawable.SetColor(Element.BackgroundColor.ToAndroid());
-        }
-
-        private void UpdateShadow()
-        {
-            if (Element.HasShadow)
-            {
-                Elevation = (float)Context.FromPixels(10);
-            }
-            else
-            {
-                Elevation = 0;
-            }
         }
     }
 }
